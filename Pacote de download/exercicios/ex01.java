eebf4a7913677ebcb786811280eb9b2dd32c043c
Hugo Henrique <hugohenrisoares@gmail.com>
2022-08-16 23:25:51 -0300

    Create ex01

---
+++ Pacote de download/exercicios/ex01.java
@@ -0,0 +1,33 @@
+package exercicios;
+
+import javax.swing.JOptionPane;
+
+
+public class ex01 {
+    
+    public static void main(String[] args) {
+        int resultado = 0;
+        int sair = 0;
+        int cont = 1;
+
+        do {
+
+            String num = JOptionPane.showInputDialog(null, "Digite o "+cont+"º numero:");
+            int numDigi= Integer.parseInt(num);
+            resultado = numDigi + resultado;
+            cont ++;
+            if(cont  > 2){
+                int escolha = JOptionPane.showConfirmDialog(null, "Resulado da soma é: "+resultado+". Deseja continuar?", "Soma", JOptionPane.YES_NO_OPTION);
+
+                sair = escolha;
+
+
+            }
+
+        } while (sair != 1);
+
+        JOptionPane.showMessageDialog(null, "Resultado da soma é: "+resultado, "Soma", JOptionPane.INFORMATION_MESSAGE);
+
+    }
+    
+}
